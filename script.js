093c06f974bd698c4445f82f1283dcbd035aa3f4
Rafael <rfl.aguilera@gmail.com>
2024-07-08 00:31:29 +0200

    New modifications

---
+++ script.js
@@ -0,0 +1,6 @@
+import * as THREE from "/node_modules/three/build/three.module.min.js";
+import * as BUI from "/node_modules/@thatopen/ui/dist/index.js";
+import * as OBC from "/node_modules/@thatopen/components/dist/index.cjs";
+import Stats from "/node_modules/stats.js/build/stats.min.js";
+
+const container = document.getElementById("container");
